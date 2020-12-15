eae0e478d32cc841cacbf962e32dcc954d79963b
Ilya <pokrovskiy.ie@gmail.com>
2020-12-15 14:02:29 +0300

    Update + cucumber (incompatible)

--- src/test/java/ru/test/cucumber/CucumberTestSteps.java
+++ src/test/java/ru/test/cucumber/CucumberTestSteps.java
@@ -8,17 +8,19 @@ import ru.test.Calculator;
 import static junit.framework.TestCase.assertEquals;
 
 public class CucumberTestSteps {
+
+    double result;
+
     @When("^выполнить операцию ([\\u002d\\u002b\\u002a\\u002f]?) с числами (-?\\d*\\.?\\d+) и (-?\\d*\\.?\\d+)$")
-    public double operation (double value1, double value2, char operation) {
-        double result = Calculator.calculator(value1, value2, operation);
-        System.out.println(result);
-        return result;
+    public void operation (char operation, String value1, String value2) {
+        double result = Calculator.calculator(Double.parseDouble(value1), Double.parseDouble(value2), operation);
+        this.result = result;
     }
 
     @Then("^результат выполнения операции равен (-?\\d*\\.?\\d+)$")
-    public void resultCompare(double result, double resultExpected) {
+    public void resultCompare(String resultExpected) {
 
-        assertEquals(resultExpected, result);
+        assertEquals(Double.parseDouble(resultExpected), result);
 
     }
 }
